2aff8c47234504c387c7a17b7803701997933ad4
Matěj Hůlek <matej@hulek.cz>
2018-05-31 09:57:53 +0200

    first commit

---
+++ src/csvtojson.ts
@@ -0,0 +1,57 @@
+
+export const csvToJson = (csvText: any, delimiter: any = findDelimiter(csvText)) => {
+    const rows = rowsSpliter(csvText);
+    return rowsRider(rows, delimiter);
+};
+
+export const rowsSpliter = (csvText: any) => {
+    return csvText.split(/[\r\n]+/).filter((v: any) => {
+        return v;
+    });
+};
+
+export const findDelimiter = (csvText: any) => {
+    const index = csvText.search(/[,;| ]/);
+    return csvText[index];
+};
+
+export const rowsRider = (rows: any, delimiter: any) => {
+    const fields = rows[0].split(delimiter);
+    const objArray = [];
+    for (let i = 1, len = rows.length; i < len; i++) {
+        objArray.push(charRider(rows[i] += delimiter, fields, delimiter));
+    }
+    return objArray;
+};
+
+export const charRider = (row: any, fields: any, delimiter: any) => {
+    let castedValue: any = '';
+    let stringDetection = 0;
+    const obj: any = {};
+    let indexCalculator = 0;
+    for (const char of row) {
+        if (char === '"') {
+            if (stringDetection === 2) {
+                stringDetection--;
+                castedValue += char;
+                continue;
+            }
+            stringDetection++;
+            continue;
+        }
+        if (char === delimiter && (stringDetection === 0 || stringDetection === 2)) {
+            obj[fields[indexCalculator]] = getCastedValue(castedValue);
+            castedValue = '';
+            indexCalculator++;
+            stringDetection = 0;
+        } else {
+            castedValue += char;
+        }
+
+    }
+    return obj;
+};
+
+export const getCastedValue = (value: any) => {
+    return value === '' ? null : isNaN(value) ? value : +value;
+};
