856ee8055e011b187970fbe3c517819678c318a1
Matěj Hůlek <matej@hulek.cz>
2018-06-01 09:33:33 +0200

    add build proces

---
+++ lib/csvtojson.d.ts
@@ -0,0 +1,6 @@
+export declare const csvToJson: (csvText: any, delimiter?: any) => any;
+export declare const rowsSpliter: (csvText: any) => any;
+export declare const findDelimiter: (csvText: any) => any;
+export declare const rowsRider: (rows: any, delimiter: any) => any;
+export declare const charRider: (row: any, fields: any, delimiter: any) => {};
+export declare const getCastedValue: (value: any) => any;
